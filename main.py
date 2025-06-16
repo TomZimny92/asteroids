f5504fad2b094c217a98320eb69e4a283b731d91
TomZimny92 <tzimny92@gmail.com>
2025-06-16 19:54:43 -0400

    first commit

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-#this allows us to use code form the open-source pygame library throughout this file
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
 import pygame
-from player import *
 from constants import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    try:
-        pygame.init()
-    except:
-        print("Game initialization failed.")
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
-
-    clock = pygame.time.Clock()
-    dt = 0
-
-    gameLoop = True
-    while gameLoop:
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        screen.fill("black")
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
-
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-
-    player.draw(screen)
 
 if __name__ == "__main__":
     main()
-
